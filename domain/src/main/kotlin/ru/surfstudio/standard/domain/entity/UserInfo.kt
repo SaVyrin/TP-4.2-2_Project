632e1fe28f809fec4b92ce0df3e4b07e654c3959
SaVyrin <sarajkinaa@gmail.com>
2023-04-18 21:59:36 +0300

    AND-20 добавлен ктонтроллер для информации о пользователе

---
+++ domain/src/main/kotlin/ru/surfstudio/standard/domain/entity/UserInfo.kt
@@ -0,0 +1,8 @@
+package ru.surfstudio.standard.domain.entity
+
+data class UserInfo(
+    val id: Int,
+    val name: String,
+    val surname: String,
+    val address: String
+)
---
+++ f-profile/src/main/java/ru/surfstudio/standard/f_profile/ui/UserInfoUi.kt
@@ -0,0 +1,9 @@
+package ru.surfstudio.standard.f_profile.ui
+
+/**
+ * Элемент с информацией о пользователе
+ * */
+data class UserInfoUi(
+    val title: String,
+    val value: String
+)
