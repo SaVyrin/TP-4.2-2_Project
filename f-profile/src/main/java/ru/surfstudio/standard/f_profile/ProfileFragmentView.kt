da01bacf4e7e9873fbad6b06d882ec38381cd2a1
SaVyrin <sarajkinaa@gmail.com>
2023-04-18 22:41:45 +0300

    AND-20 добавлен decoration для элементов с информацией о пользователе

--- f-profile/src/main/java/ru/surfstudio/standard/f_profile/ProfileFragmentView.kt
+++ f-profile/src/main/java/ru/surfstudio/standard/f_profile/ProfileFragmentView.kt
@@ -9,10 +9,14 @@ import ru.surfstudio.android.core.mvi.impls.event.hub.ScreenEventHub
 import ru.surfstudio.android.core.ui.navigation.feature.route.feature.CrossFeatureFragment
 import ru.surfstudio.android.core.ui.view_binding.viewBinding
 import ru.surfstudio.android.easyadapter.EasyAdapter
+import ru.surfstudio.android.recycler.decorator.Decorator
+import ru.surfstudio.android.recycler.decorator.MasterDecorator
 import ru.surfstudio.standard.f_profile.controller.UserInfoController
 import ru.surfstudio.standard.f_profile.databinding.FragmentProfileBinding
 import ru.surfstudio.standard.f_profile.di.ProfileScreenConfigurator
 import ru.surfstudio.standard.ui.mvi.view.BaseMviFragmentView
+import ru.surfstudio.standard.ui.recylcer.SimpleDecor
+import ru.surfstudio.standard.ui.util.toPx
 import javax.inject.Inject
 
 /**
@@ -47,6 +51,7 @@ internal class ProfileFragmentView : BaseMviFragmentView<ProfileState, ProfileEv
             with(profileRv) {
                 adapter = easyAdapter
                 layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(buildDecoration())
             }
         }
     }
@@ -55,7 +60,15 @@ internal class ProfileFragmentView : BaseMviFragmentView<ProfileState, ProfileEv
         easyAdapter.setData(state.screenItems, userInfoController)
     }
 
-    private fun buildDecoration() {
-        // TODO добавить decoration
+    private fun buildDecoration(): MasterDecorator {
+        val userInfoDecoration = SimpleDecor(
+            top = 20.toPx,
+            left = 16.toPx,
+            right = 16.toPx
+        )
+
+        return Decorator.Builder()
+            .offset(userInfoController.viewType() to userInfoDecoration)
+            .build()
     }
 }
