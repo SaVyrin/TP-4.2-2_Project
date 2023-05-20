28173c95b31111a4c3dfd26723e2e4735af40a6c
SaVyrin <sarajkinaa@gmail.com>
2023-05-20 15:23:08 +0300

    AND-12 добавлена логика запросов с ИПУ

--- f-metrics/src/main/java/ru/surfstudio/standard/f_metrics/MetricsMiddleware.kt
+++ f-metrics/src/main/java/ru/surfstudio/standard/f_metrics/MetricsMiddleware.kt
@@ -4,20 +4,43 @@ import io.reactivex.Observable
 import ru.surfstudio.android.core.mvi.impls.ui.middleware.BaseMiddleware
 import ru.surfstudio.android.core.mvi.impls.ui.middleware.BaseMiddlewareDependency
 import ru.surfstudio.android.dagger.scope.PerScreen
-import ru.surfstudio.standard.f_metrics.MetricsEvent.Navigation
+import ru.surfstudio.standard.domain.entity.UserInfo
+import ru.surfstudio.standard.f_metrics.MetricsEvent.*
+import ru.surfstudio.standard.i_ipu.IpuInteractor
+import ru.surfstudio.standard.i_user.UserStorage
 import ru.surfstudio.standard.ui.mvi.navigation.base.NavigationMiddleware
 import javax.inject.Inject
 
 @PerScreen
 internal class MetricsMiddleware @Inject constructor(
     basePresenterDependency: BaseMiddlewareDependency,
-    private val navigationMiddleware: NavigationMiddleware
+    private val navigationMiddleware: NavigationMiddleware,
+    private val userStorage: UserStorage,
+    private val ipuInteractor: IpuInteractor,
+    private val sh: MetricsScreenStateHolder
 ) : BaseMiddleware<MetricsEvent>(basePresenterDependency) {
+
+    private val state: MetricsState get() = sh.value
 
     override fun transform(eventStream: Observable<MetricsEvent>): Observable<out MetricsEvent> =
         transformations(eventStream) {
             addAll(
-                Navigation::class decomposeTo navigationMiddleware
+                onCreate() eventMap { handleOnCreate() },
+                Navigation::class decomposeTo navigationMiddleware,
+                Input.SendIpuClicked::class eventMapTo { handleSendIpuClicked() }
             )
         }
+
+    private fun handleOnCreate(): Observable<out CurrentIpuRequestEvent> {
+        val user = userStorage.currentUser ?: UserInfo.EMPTY_USER
+        return ipuInteractor.getCurrentIpu(user.id.toString())
+            .io()
+            .asRequestEvent { CurrentIpuRequestEvent(it) }
+    }
+
+    private fun handleSendIpuClicked(): Observable<out SendIpuRequestEvent> {
+        return ipuInteractor.sendIpu(state.metricsUiItems)
+            .io()
+            .asRequestEvent { SendIpuRequestEvent(it) }
+    }
 }
--- f-metrics/src/main/java/ru/surfstudio/standard/f_metrics/MetricsReducer.kt
+++ f-metrics/src/main/java/ru/surfstudio/standard/f_metrics/MetricsReducer.kt
@@ -2,11 +2,21 @@ package ru.surfstudio.standard.f_metrics
 
 import ru.surfstudio.android.core.mvi.impls.ui.reactor.BaseReactorDependency
 import ru.surfstudio.android.core.mvi.impls.ui.reducer.BaseReducer
+import ru.surfstudio.android.core.mvi.ui.mapper.RequestMapper
+import ru.surfstudio.android.core.mvp.binding.rx.relation.mvp.Command
 import ru.surfstudio.android.core.mvp.binding.rx.relation.mvp.State
+import ru.surfstudio.android.core.mvp.binding.rx.request.data.RequestUi
+import ru.surfstudio.android.core.ui.provider.resource.ResourceProvider
 import ru.surfstudio.android.dagger.scope.PerScreen
+import ru.surfstudio.standard.domain.entity.Ipu
+import ru.surfstudio.standard.f_metrics.MetricsEvent.*
+import ru.surfstudio.standard.ui.mvi.mappers.RequestMappers
 import javax.inject.Inject
 
-internal class MetricsState
+internal data class MetricsState(
+    val ipuRequestUi: RequestUi<List<Ipu>> = RequestUi(),
+    val metricsUiItems: List<Ipu> = emptyList()
+)
 
 /**
  * State Holder [MetricsFragmentView]
@@ -15,15 +25,56 @@ internal class MetricsState
 internal class MetricsScreenStateHolder @Inject constructor(
 ) : State<MetricsState>(MetricsState())
 
+@PerScreen
+internal class IpuCommandHolder @Inject constructor() {
+    val errorMessage = Command<String>()
+}
+
 /**
  * Reducer [MetricsFragmentView]
  */
 @PerScreen
 internal class MetricsReducer @Inject constructor(
-    dependency: BaseReactorDependency
+    dependency: BaseReactorDependency,
+    private val ch: IpuCommandHolder,
+    private val resourceProvider: ResourceProvider
 ) : BaseReducer<MetricsEvent, MetricsState>(dependency) {
 
     override fun reduce(state: MetricsState, event: MetricsEvent): MetricsState {
-        return state
+        return when (event) {
+            is CurrentIpuRequestEvent -> handleIpuRequestEvent(state, event)
+            is Input.IpuChanged -> handleIpuChanged(state, event)
+            else -> state
+        }
+    }
+
+    private fun handleIpuRequestEvent(state: MetricsState, event: CurrentIpuRequestEvent): MetricsState {
+        val request = RequestMapper.builder(event.request)
+            .mapData(RequestMappers.data.default())
+            .mapLoading(RequestMappers.loading.simple())
+            .handleError(RequestMappers.error.noInternet(errorHandler))
+            .handleError { _, _, _ ->
+                val message = resourceProvider.getString(R.string.default_error_message)
+                ch.errorMessage.accept(message)
+                true
+            }
+            .build()
+
+        val ipu = request.data ?: emptyList()
+        return state.copy(
+            ipuRequestUi = request,
+            metricsUiItems = ipu
+        )
+    }
+
+    private fun handleIpuChanged(state: MetricsState, event: Input.IpuChanged): MetricsState {
+        val metricsUiItems = state.metricsUiItems.map {
+            if (it.id == event.ipu.id) {
+                event.ipu
+            } else {
+                it
+            }
+        }
+        return state.copy(metricsUiItems = metricsUiItems)
     }
 }
