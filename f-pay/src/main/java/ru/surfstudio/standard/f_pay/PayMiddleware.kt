fa9122aab79f35d734d874ad2d37a3958e6e59f7
SaVyrin <sarajkinaa@gmail.com>
2023-05-20 22:52:13 +0300

    AND-13 добавлена логика запроса данных для экрана оплаты

--- f-pay/src/main/java/ru/surfstudio/standard/f_pay/PayMiddleware.kt
+++ f-pay/src/main/java/ru/surfstudio/standard/f_pay/PayMiddleware.kt
@@ -4,20 +4,56 @@ import io.reactivex.Observable
 import ru.surfstudio.android.core.mvi.impls.ui.middleware.BaseMiddleware
 import ru.surfstudio.android.core.mvi.impls.ui.middleware.BaseMiddlewareDependency
 import ru.surfstudio.android.dagger.scope.PerScreen
-import ru.surfstudio.standard.f_pay.PayEvent.Navigation
+import ru.surfstudio.standard.domain.entity.UserInfo
+import ru.surfstudio.standard.f_pay.PayEvent.*
+import ru.surfstudio.standard.i_ipu.IpuInteractor
+import ru.surfstudio.standard.i_pay.PayInteractor
+import ru.surfstudio.standard.i_user.UserStorage
 import ru.surfstudio.standard.ui.mvi.navigation.base.NavigationMiddleware
 import javax.inject.Inject
 
 @PerScreen
 internal class PayMiddleware @Inject constructor(
     basePresenterDependency: BaseMiddlewareDependency,
-    private val navigationMiddleware: NavigationMiddleware
+    private val navigationMiddleware: NavigationMiddleware,
+    private val ipuInteractor: IpuInteractor,
+    private val payInteractor: PayInteractor,
+    private val userStorage: UserStorage
 ) : BaseMiddleware<PayEvent>(basePresenterDependency) {
 
     override fun transform(eventStream: Observable<PayEvent>): Observable<out PayEvent> =
         transformations(eventStream) {
             addAll(
-                Navigation::class decomposeTo navigationMiddleware
+                onResume() eventMap { getCurrentIpu() },
+                Navigation::class decomposeTo navigationMiddleware,
+                GetCurrentIpuRequestEvent::class filter { it.isSuccess } eventMap { getPayments() },
             )
         }
+
+    private fun getCurrentIpu(): Observable<out PayEvent> {
+        val user = userStorage.currentUser ?: UserInfo.EMPTY_USER
+        return ipuInteractor.getCurrentIpu(user.id.toString())
+            .io()
+            .asRequestEvent(::GetCurrentIpuRequestEvent)
+    }
+
+    private fun getPayments(): Observable<out PayEvent> {
+        val user = userStorage.currentUser ?: UserInfo.EMPTY_USER
+        return merge(
+            payInteractor.getPayments(user.id.toString())
+                .io()
+                .asRequestEvent(::GetPaymentsRequestEvent),
+
+            payInteractor.getExpectedPayment(user.id.toString())
+                .io()
+                .asRequestEvent(::GetExpectedPaymentRequestEvent),
+        )
+    }
+
+    private fun handlePayClicked(): Observable<out PayEvent> {
+        val user = userStorage.currentUser ?: UserInfo.EMPTY_USER
+        return payInteractor.pay(user.id.toString())
+            .io()
+            .asRequestEvent(::PayRequestEvent)
+    }
 }
--- f-pay/src/main/java/ru/surfstudio/standard/f_pay/PayReducer.kt
+++ f-pay/src/main/java/ru/surfstudio/standard/f_pay/PayReducer.kt
@@ -2,11 +2,21 @@ package ru.surfstudio.standard.f_pay
 
 import ru.surfstudio.android.core.mvi.impls.ui.reactor.BaseReactorDependency
 import ru.surfstudio.android.core.mvi.impls.ui.reducer.BaseReducer
+import ru.surfstudio.android.core.mvi.ui.mapper.RequestMapper
+import ru.surfstudio.android.core.mvp.binding.rx.relation.mvp.Command
 import ru.surfstudio.android.core.mvp.binding.rx.relation.mvp.State
+import ru.surfstudio.android.core.ui.provider.resource.ResourceProvider
 import ru.surfstudio.android.dagger.scope.PerScreen
+import ru.surfstudio.android.utilktx.ktx.text.EMPTY_STRING
+import ru.surfstudio.standard.f_pay.PayEvent.*
+import ru.surfstudio.standard.ui.mvi.mappers.RequestMappers
 import javax.inject.Inject
 
-internal class PayState
+internal data class PayState(
+    val payUiItems: List<PayUi> = emptyList(),
+    val paymentSummaryText: String = EMPTY_STRING,
+    val expectedPaymentText: String = EMPTY_STRING
+)
 
 /**
  * State Holder [PayFragmentView]
@@ -15,15 +25,117 @@ internal class PayState
 internal class PayScreenStateHolder @Inject constructor(
 ) : State<PayState>(PayState())
 
+@PerScreen
+internal class PayCommandHolder @Inject constructor() {
+    val errorMessage = Command<String>()
+    val paySuccess = Command<String>()
+}
+
 /**
  * Reducer [PayFragmentView]
  */
 @PerScreen
 internal class PayReducer @Inject constructor(
-    dependency: BaseReactorDependency
+    dependency: BaseReactorDependency,
+    private val resourceProvider: ResourceProvider,
+    private val ch: PayCommandHolder
 ) : BaseReducer<PayEvent, PayState>(dependency) {
 
     override fun reduce(state: PayState, event: PayEvent): PayState {
-        return state
+        return when (event) {
+            is GetCurrentIpuRequestEvent -> handleGetCurrentIpuRequestEvent(state, event)
+            is GetPaymentsRequestEvent -> handleGetPaymentsRequestEvent(state, event)
+            is GetExpectedPaymentRequestEvent -> handleGetExpectedPaymentRequestEvent(state, event)
+            else -> state
+        }
+    }
+
+    private fun handleGetCurrentIpuRequestEvent(
+        state: PayState,
+        event: GetCurrentIpuRequestEvent
+    ): PayState {
+        val request = RequestMapper.builder(event.request)
+            .mapData(RequestMappers.data.default())
+            .mapLoading(RequestMappers.loading.simple())
+            .handleError(RequestMappers.error.noInternet(errorHandler))
+            .handleError { _, _, _ ->
+                val message = resourceProvider.getString(R.string.default_error_message)
+                ch.errorMessage.accept(message)
+                true
+            }
+            .build()
+
+        val ipu = request.data ?: emptyList()
+        val payUiItems = ipu.map {
+            PayUi(it, "")
+        }
+        return state.copy(payUiItems = payUiItems)
+    }
+
+    private fun handleGetPaymentsRequestEvent(
+        state: PayState,
+        event: GetPaymentsRequestEvent
+    ): PayState {
+        val request = RequestMapper.builder(event.request)
+            .mapData(RequestMappers.data.default())
+            .mapLoading(RequestMappers.loading.simple())
+            .handleError(RequestMappers.error.noInternet(errorHandler))
+            .handleError { _, _, _ ->
+                val message = resourceProvider.getString(R.string.default_error_message)
+                ch.errorMessage.accept(message)
+                true
+            }
+            .build()
+
+        val payments = request.data ?: emptyList()
+        val payUiItems = state.payUiItems.map { payUi ->
+            val payment = payments.find { payment ->
+                payment.type == payUi.ipu.type
+            }
+            if (payment != null) {
+                val payAmount = resourceProvider.getString(
+                    R.string.pay_single_metric_text,
+                    payment.value
+                )
+                payUi.copy(payAmount = payAmount)
+            } else {
+                payUi
+            }
+        }
+        val paymentSummary = payments.sumOf { it.value }
+        val paymentSummaryText = resourceProvider.getString(
+            R.string.pay_summary_text,
+            paymentSummary
+        )
+        return state.copy(
+            payUiItems = payUiItems,
+            paymentSummaryText = paymentSummaryText
+        )
+    }
+
+    private fun handleGetExpectedPaymentRequestEvent(
+        state: PayState,
+        event: GetExpectedPaymentRequestEvent
+    ): PayState {
+        val request = RequestMapper.builder(event.request)
+            .mapData(RequestMappers.data.default())
+            .mapLoading(RequestMappers.loading.simple())
+            .handleError(RequestMappers.error.noInternet(errorHandler))
+            .handleError { _, _, _ ->
+                val message = resourceProvider.getString(R.string.default_error_message)
+                ch.errorMessage.accept(message)
+                true
+            }
+            .build()
+
+        val expectedPaymentText = if (request.data != null) {
+            resourceProvider.getString(
+                R.string.pay_next_period_text,
+                request.data ?: 0
+            )
+        } else {
+            EMPTY_STRING
+        }
+        return state.copy(expectedPaymentText = expectedPaymentText)
     }
 }
